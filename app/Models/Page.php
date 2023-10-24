ea79b4f2a94f8b0252f7526faa5900078ad1f662
MaximBrewer <pimax1978@icloud.com>
2023-10-24 19:30:22 +0600

    Fix Page Model

---
+++ app/Models/Page.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    /**
+     * Statuses.
+     */
+    public const STATUS_ACTIVE = 'ACTIVE';
+    public const STATUS_INACTIVE = 'INACTIVE';
+
+    /**
+     * List of statuses.
+     *
+     * @var array
+     */
+    public static $statuses = [self::STATUS_ACTIVE, self::STATUS_INACTIVE];
+}
